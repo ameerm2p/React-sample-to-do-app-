f1e43ac822aa119cdf772fd5a0c232e67678ee11
ameerm2p <ameer.basha@m2pfintech.com>
2023-12-05 12:37:39 +0530

    to do functionalities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function App() {
+  const [Arrdata, setArrData] = useState([]);
+  const [inputData, setInputData] = useState("");
+  const [buttonText, setButtonText] = useState("Save");
+  const [indexVal, setIndexVal] = useState(0);
+  function submitButton() {
+    if (buttonText === "Save") {
+      if (inputData != "") {
+        setArrData((pre) => [...pre, inputData]);
+        setInputData("");
+      } else {
+        alert("Please enter a text");
+      }
+    } else {
+      console.log(indexVal);
+      const updateData = [...Arrdata];
+      updateData[indexVal] = inputData;
+      setArrData(updateData);
+      setInputData("");
+      setButtonText("Save");
+    }
+  }
+  // console.log("data- "+Arrdata);
+  function inputChange(e) {
+    //console.log(e.target.value);
+    setInputData(e.target.value);
+  }
+
+  function editText(e, index) {
+    setInputData(e);
+
+    setIndexVal(index);
+    setButtonText("Edit");
+  }
+
+  function deleteText(index) {
+    // console.log(index);
+    const newData = Arrdata.filter((_, i) => i !== index);
+    setArrData(newData);
+  }
+
+  function handleCheckboxChange(e, index) {
+    console.log("id: " + index + " - check: " + e.target.checked);
+   
+  }
+  
+  function handelAllDeleteButton() {
+    setArrData([]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form.Label htmlFor="inputPassword5">Text:</Form.Label>
+      <br></br>
+      <Form.Control
+        type="text"
+        id="data"
+        name="data"
+        aria-describedby="passwordHelpBlock"
+        style={{ width: "200px", display: "initial", cursor: "pointer" }}
+        value={inputData}
+        onChange={inputChange}
+      />
+      <br></br>
+
+      <Button variant="primary" onClick={submitButton} type="submit">
+        Submit
+      </Button>
+      <Button variant="danger" onClick={handelAllDeleteButton} type="submit">
+        Delete All
+      </Button>
+   
+      <br></br>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Text</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Arrdata.map((item, index) => (
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  onChange={(e) => handleCheckboxChange(e, index)}
+                />
+              </td>
+              <td>{item}</td>
+              <td>
+                <button
+                  type="submit"
+                  id={"edit-" + index}
+                  onClick={() => editText(item, index)}
+                >
+                  Edit
+                </button>
+              </td>
+              <td>
+                <button
+                  type="submit"
+                  id={"delete-" + index}
+                  onClick={() => deleteText(index)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
